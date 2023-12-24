8f4c31d897b2d8dd4f0b28b9f46f6bd264a0b823
melekabbassi <abbassimelek@gmail.com>
2023-12-24 20:41:49 +0100

    listed pecs exercices and changed fixed favorite button

--- lib/pages/tabs/home_page_tabs/pecs_tab.dart
+++ lib/pages/tabs/home_page_tabs/pecs_tab.dart
@@ -1,12 +1,72 @@
+import 'package:fitnessapp/components/exercice_card.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class PecsTab extends StatelessWidget {
+class PecsTab extends StatefulWidget {
   const PecsTab({super.key});
 
   @override
+  State<PecsTab> createState() => _PecsTabState();
+}
+
+class _PecsTabState extends State<PecsTab> {
+  final _pecsExercicesStream = Supabase.instance.client
+      .from('exercices')
+      .stream(primaryKey: ['id']).eq('category', 'pecs');
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Pecs"),
+    return Scaffold(
+      body: StreamBuilder<List<Map<String, dynamic>>>(
+        stream: _pecsExercicesStream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final exercices = snapshot.data!;
+          return ListView.builder(
+            itemCount: exercices.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Row(
+                  children: [
+                    Text(exercices[index]['name']),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () async {
+                        // change favorite to true if false and vice versa
+                        final favorite = exercices[index]['favorite'];
+                        await Supabase.instance.client
+                            .from('exercices')
+                            .update({'favorite': !favorite}).eq(
+                                'id', exercices[index]['id']);
+                      },
+                      // if favorite is true, show filled star, else show empty star
+                      icon: exercices[index]['favorite']
+                          ? const Icon(
+                              Icons.favorite,
+                              color: Colors.lightGreenAccent,
+                            )
+                          : const Icon(
+                              Icons.favorite_border,
+                              color: Colors.lightGreenAccent,
+                            ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
+    // return Scaffold(
+    //   body: ListView.builder(
+    //     itemCount: 10,
+    //     itemBuilder: (context, index) {
+    //       return const ExerciceCard();
+    //     },
+    //   ),
+    // );
   }
 }
